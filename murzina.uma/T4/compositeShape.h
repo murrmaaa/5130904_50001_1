85f60ac03259f9c5e09e32b7e09aa129bca057cd
murrmaaa <umamurochka@gmail.com>
2026-03-18 22:53:16 +0300

    Create compositeShape.h

---
+++ murzina.uma/T4/compositeShape.h
@@ -0,0 +1,25 @@
+#ifndef COMPOSITE_SHAPE_H
+#define COMPOSITE_SHAPE_H
+
+#include <vector>
+#include <memory>
+#include "shape.h"
+
+class CompositeShape : public Shape
+{
+private:
+    std::vector<std::unique_ptr<Shape>> shapes_;
+
+public:
+    void addShape(std::unique_ptr<Shape> shape);
+
+    double getArea() const override;
+    Point getCenter() const override;
+
+    void move(double dx, double dy) override;
+    void scale(double k) override;
+
+    std::string getName() const override;
+};
+
+#endif
