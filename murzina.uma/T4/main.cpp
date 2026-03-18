0aed76a61a0e607d92b5823b1d2fa9b6a20c1bdd
murrmaaa <umamurochka@gmail.com>
2026-03-18 23:04:30 +0300

    fix main.cpp

--- murzina.uma/T4/main.cpp
+++ murzina.uma/T4/main.cpp
@@ -9,7 +9,9 @@
 void print(const Shape& s)
 {
     Point c = s.getCenter();
-    std::cout << "[" << s.getName() << ", (" << c.x << ", " << c.y << "), " << s.getArea() << "]\n";
+    std::cout << "[" << s.getName()
+              << ", (" << c.x << ", " << c.y << "), "
+              << s.getArea() << "]\n";
 }
 
 int main()
@@ -19,22 +21,29 @@ int main()
     shapes.push_back(std::make_unique<Rectangle>(Point{0, 0}, 4, 3));
     shapes.push_back(std::make_unique<Trapezoid>(Point{0, 0}, 6, 4, 3));
 
-    CompositeShape comp;
-    comp.addShape(std::make_unique<Rectangle>(Point{1, 1}, 2, 2));
-    comp.addShape(std::make_unique<Trapezoid>(Point{0, 0}, 4, 2, 2));
+    auto comp = std::make_unique<CompositeShape>();
 
-    shapes.push_back(std::make_unique<CompositeShape>(comp));
+    comp->addShape(std::make_unique<Rectangle>(Point{1, 1}, 2, 2));
+    comp->addShape(std::make_unique<Trapezoid>(Point{0, 0}, 4, 2, 2));
+
+    shapes.push_back(std::move(comp));
 
     std::cout << "Before scale:\n";
     for (const auto& s : shapes)
+    {
         print(*s);
+    }
 
     for (auto& s : shapes)
+    {
         s->scale(2);
+    }
 
     std::cout << "\nAfter scale:\n";
     for (const auto& s : shapes)
+    {
         print(*s);
+    }
 
     return 0;
 }
