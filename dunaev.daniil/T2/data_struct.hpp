d5353d02d7353c2214e910a429e827091aba2e68
x1tinter <dunaevdan666@gmail.com>
2026-04-07 19:05:22 +0300

    Create data_struct.hpp

---
+++ dunaev.daniil/T2/data_struct.hpp
@@ -0,0 +1,19 @@
+#ifndef DATA_STRUCT_HPP
+#define DATA_STRUCT_HPP
+
+#include <iosfwd>
+#include <string>
+
+struct DataStruct
+{
+  double key1;
+  unsigned long long key2;
+  std::string key3;
+};
+
+bool compareDataStruct(const DataStruct& lhs, const DataStruct& rhs);
+
+std::istream& operator>>(std::istream& in, DataStruct& value);
+std::ostream& operator<<(std::ostream& out, const DataStruct& value);
+
+#endif
