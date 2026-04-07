1225fda55ee45818807256407daaf8fd985c6607
x1tinter <dunaevdan666@gmail.com>
2026-04-07 19:04:34 +0300

    Create main.cpp

---
+++ dunaev.daniil/T2/main.cpp
@@ -0,0 +1,23 @@
+#include "data_struct.hpp"
+
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main()
+{
+  std::vector< DataStruct > data;
+
+  std::copy(
+      std::istream_iterator< DataStruct >(std::cin),
+      std::istream_iterator< DataStruct >(),
+      std::back_inserter(data));
+
+  std::sort(data.begin(), data.end(), compareDataStruct);
+
+  std::copy(
+      data.cbegin(),
+      data.cend(),
+      std::ostream_iterator< DataStruct >(std::cout, "\n"));
+}
