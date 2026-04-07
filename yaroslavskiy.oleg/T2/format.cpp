1cd39b722cfc7b023992a7be54fd1a72fff97803
Dragon Legend <yars.oleg@mail.ru>
2026-04-07 23:21:19 +0300

    Fix format cpp

--- yaroslavskiy.oleg/T2/format.cpp
+++ yaroslavskiy.oleg/T2/format.cpp
@@ -5,11 +5,11 @@
 
 std::string formatLongLongLit(long long value)
 {
-	return std::to_string(value) + "LL";
+  return std::to_string(value) + "LL";
 }
 
 std::string formatRational(const std::pair<long long, unsigned long long>& value)
 {
-	return "(:N " + std::to_string(value.first) + ":D " + std::to_string(value.second) + ":)";
+  return "(:N " + std::to_string(value.first) + ":D " + std::to_string(value.second) + ":)";
 }
 
