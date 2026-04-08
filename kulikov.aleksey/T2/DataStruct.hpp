72bc7563472e8c029e51a7bb337930fc396033b5
Schnelleq <kulikow.alexey07@gmail.com>
2026-04-08 21:21:53 +0300

    Update DataStruct.hpp

--- kulikov.aleksey/T2/DataStruct.hpp
+++ kulikov.aleksey/T2/DataStruct.hpp
@@ -1,1 +1,23 @@
+#ifndef DATASTRUCT_HPP
+#define DATASTRUCT_HPP
 
+#include <iostream>
+#include <string>
+#include <complex>
+
+struct DataStruct
+{
+    unsigned long long key1;
+    std::complex<double> key2;
+    std::string key3;
+};
+
+unsigned long long parseBinary(const std::string& s);
+std::string toBinaryString(unsigned long long n);
+bool tryParseRecord(const std::string& record, DataStruct& ds);
+bool comparator(const DataStruct& a, const DataStruct& b);
+
+std::istream& operator>>(std::istream& in, DataStruct& ds);
+std::ostream& operator<<(std::ostream& out, const DataStruct& ds);
+
+#endif
