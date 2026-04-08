5e363ede4478e484ef2d76bab40e38a4389fd67b
Schnelleq <kulikow.alexey07@gmail.com>
2026-04-08 21:23:07 +0300

    Update main.cpp

--- kulikov.aleksey/T2/main.cpp
+++ kulikov.aleksey/T2/main.cpp
@@ -1,1 +1,26 @@
+#include "DataStruct.hpp"
 
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int main()
+{
+    std::vector<DataStruct> data;
+
+    std::copy(
+        std::istream_iterator<DataStruct>(std::cin),
+        std::istream_iterator<DataStruct>(),
+        std::back_inserter(data)
+    );
+
+    std::sort(data.begin(), data.end(), comparator);
+
+    std::copy(
+        data.begin(),
+        data.end(),
+        std::ostream_iterator<DataStruct>(std::cout, "\n")
+    );
+  
+    return EXIT_SUCCES;
+}
