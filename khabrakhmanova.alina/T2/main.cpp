84d5e93247a36ecad61d231f980f44d4c45576bd
AkiraSemidzu <akirasemidzu@gmail.com>
2026-04-09 02:39:21 +0300

    Add full solution for T2(10)

--- khabrakhmanova.alina/T2/main.cpp
+++ khabrakhmanova.alina/T2/main.cpp
@@ -8,22 +8,11 @@
 int main() {
     std::vector<DataStruct> data;
 
-    // Проверяем, есть ли вообще какие-то данные во входном потоке
-    if (std::cin.peek() == EOF) {
-        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped" << std::endl;
-        return 0;
-    }
-
     std::copy(
         std::istream_iterator<DataStruct>(std::cin),
         std::istream_iterator<DataStruct>(),
         std::back_inserter(data)
     );
-
-    if (data.empty()) {
-        std::cout << "Atleast one supported record type" << std::endl;
-        return 0;
-    }
 
     std::sort(data.begin(), data.end(), compareDataStruct);
 
@@ -33,5 +22,5 @@ int main() {
         std::ostream_iterator<DataStruct>(std::cout, "\n")
     );
 
-    return 0;
+    return EXIT_SUCCESS;
 }
