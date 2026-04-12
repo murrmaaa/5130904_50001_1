ca2f35635beccca88e425618b68dab0c51634100
Filatov Andrey <2448645@mail.ru>
2026-04-13 00:39:17 +0300

    переделаный вариант с выводом и данных и сообщения

--- filatov.andrey/T2/main.cpp
+++ filatov.andrey/T2/main.cpp
@@ -1,34 +1,38 @@
-#include <iostream>
-#include <vector>
+// main.cpp
 #include <algorithm>
+#include <iostream>
 #include <iterator>
+#include <vector>
+#include <string>
 #include <sstream>
 #include "data_struct.h"
 
-struct Line {
-    std::string str;
-};
-
-std::istream& operator>>(std::istream& in, Line& line) {
-    return std::getline(in, line.str);
-}
-
 int main() {
-    std::vector<DataStruct> vec;
-    std::istream_iterator<Line> in_iter(std::cin), eof;
-    std::for_each(in_iter, eof, [&](const Line& line) {
-        std::istringstream iss(line.str);
-        DataStruct temp;
-        if (iss >> temp) {
-            vec.push_back(temp);
+    std::vector<DataStruct> data;
+    std::string line;
+
+    // Чтение через getline + istringstream + operator>> (игнорируем строки с неверным форматом)
+    while (std::getline(std::cin, line)) {
+        if (line.empty()) continue;
+        std::istringstream iss(line);
+        DataStruct ds;
+        if (iss >> ds) {
+            data.push_back(std::move(ds));
         }
+    }
+
+    // Сортировка по требованиям (key1, |key2|, длина key3)
+    std::sort(data.begin(), data.end(), [](const DataStruct& a, const DataStruct& b) {
+        if (a.key1 != b.key1) return a.key1 < b.key1;
+        double moda = std::abs(a.key2);
+        double modb = std::abs(b.key2);
+        if (moda != modb) return moda < modb;
+        return a.key3.size() < b.key3.size();
     });
 
-    if (vec.empty()) {
-        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped";
-    } else {
-        std::cout << "Atleast one supported record type";
-    }
+    // Вывод через std::copy + ostream_iterator (точно по условию задания)
+    std::copy(data.begin(), data.end(),
+              std::ostream_iterator<DataStruct>(std::cout, "\n"));
 
     return 0;
 }
